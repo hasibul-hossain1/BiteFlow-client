ef5ff6eb090d3c982d0de05c8e4373906392ab55
shanto <shanto22e@gmail.com>
2025-07-01 23:31:39 +0600

    feat: Add sorting options to AllFoods component

--- src/pages/AllFoods.jsx
+++ src/pages/AllFoods.jsx
@@ -7,14 +7,31 @@ import Pattern from "../components/Common/Pattern";
 
 function AllFoods() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [filter, setFilter] = useState("");
+  const [sort, setSort] = useState("");
   const foods = useSelector((state) => state.foods);
-  const filterFoods =
+
+  const foodCategories = [
+    ...new Set(foods?.data?.map((food) => food.foodCategory)),
+  ];
+
+  let filterFoods =
     foods.data.length &&
     [...foods.data]
       .reverse()
       .filter((item) =>
-        item.foodName.toLowerCase().includes(searchTerm.toLowerCase())
-      );
+        item.foodName.toLowerCase().includes(searchTerm.toLowerCase()) &&  filter?item.foodCategory===filter:true
+      )
+      .sort((a, b) => {
+        if (sort === "name") {
+          return a.foodName.localeCompare(b.foodName);
+        } else if (sort === "purchaseCount") {
+          return b.purchaseCount - a.purchaseCount; // Assuming higher purchaseCount is better
+        }
+        return 0;
+      });
+
+
   const cardVariants = {
     hidden: { opacity: 0, y: 30 },
     visible: (i) => ({
@@ -31,21 +48,44 @@ function AllFoods() {
     <section className="mt-30 max-w-6xl mx-auto">
       <Pattern>
         <motion.h3
-        initial={{ opacity: 0, y: -20 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6 }}
-        viewport={{ once: true, amount:0.2 }}
-        className="text-3xl md:text-4xl text-center font-bold mb-6"
-      >
-        All Foods
-      </motion.h3>
-      <p className="mt-5 text-center">
-        From classic favorites to new delights — discover every delicious item
-        we proudly serve.
-      </p>
+          initial={{ opacity: 0, y: -20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6 }}
+          viewport={{ once: true, amount: 0.2 }}
+          className="text-3xl md:text-4xl text-center font-bold mb-6"
+        >
+          All Foods
+        </motion.h3>
+        <p className="mt-5 text-center">
+          From classic favorites to new delights — discover every delicious item
+          we proudly serve.
+        </p>
       </Pattern>
       <div className="divider"></div>
-      <div className="text-center mt-5">
+      <div className="flex justify-between items-end flex-wrap mt-5">
+        <div className="">
+          <span>Filter Category</span>
+          <select
+            defaultValue="All"
+            onChange={e=>setFilter(e.target.value)}
+            className="select rounded-xs border-2"
+          >
+            <option value="">All</option>
+            {foodCategories?.map((item,index)=><option key={index} value={item}>{item}</option>)}
+          </select>
+        </div>
+        <div className="">
+          <span>Sort By</span>
+          <select
+            defaultValue=""
+            onChange={e=>setSort(e.target.value)}
+            className="select rounded-xs border-2"
+          >
+            <option value="">None</option>
+            <option value="name">Name</option>
+            <option value="purchaseCount">Purchase Count</option>
+          </select>
+        </div>
         <label className="input border-2">
           <svg
             className="h-[1em] opacity-50"
@@ -72,6 +112,7 @@ function AllFoods() {
           />
         </label>
       </div>
+
       <div className="grid grid-cols-1 gap-8 justify-items-center mt-20 md:grid-cols-2 xl:grid-cols-3">
         {filterFoods.length ? (
           filterFoods.map((item, i) => {
